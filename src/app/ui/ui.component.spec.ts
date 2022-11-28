4e46c480d715a8f0492bb2e68e2c065168fe12dd
btoarriola <betokanon@gmail.com>
2022-11-28 10:25:31 -0600

    create branch resta

--- src/app/ui/ui.component.spec.ts
+++ src/app/ui/ui.component.spec.ts
@@ -101,3 +101,101 @@ describe('Ui Addition - Component', () => {
 
 });
 
+
+describe('Ui Substraction - Component', () => {
+  let componentsub: UiComponent;
+  let fixturesub: ComponentFixture<UiComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ UiComponent ],
+      imports: [FormsModule],
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixturesub = TestBed.createComponent(UiComponent);
+    componentsub = fixturesub.componentInstance;
+    fixturesub.detectChanges();
+  });
+
+  it('Should call substraction method', () => {
+     // Arrange
+     let result = 0;
+     componentsub.operator1 = 2;
+     componentsub.operator2 = 2;
+ 
+     // Act
+     componentsub.substraction();
+     result = componentsub.result;
+ 
+     // Assert
+     expect(result).toBe(0);
+  });
+
+
+
+  it('Should set operator1 model through ngModel', async() => {
+    // Arrange 
+    await fixturesub.whenStable();
+    fixturesub.detectChanges();
+    const inputElement = fixturesub.debugElement.query(By.css('input[name="operator1"]')).nativeElement;
+
+    // Act 
+    inputElement.value = '3.1416';
+    inputElement.dispatchEvent(new Event('input'));
+    fixturesub.detectChanges();
+
+    // Assert 
+    expect(componentsub.operator1).toEqual(3.1416);
+  });
+ 
+  it('Should set operator2 model through ngModel', async() => {
+    // Arrange 
+    await fixturesub.whenStable();
+    fixturesub.detectChanges();
+    const inputElement = fixturesub.debugElement.query(By.css('input[name="operator2"]')).nativeElement;
+
+    // Act 
+    inputElement.value = '2.71';
+    inputElement.dispatchEvent(new Event('input'));
+    fixturesub.detectChanges();
+
+    // Assert 
+    expect(componentsub.operator2).toEqual(2.71);
+  });
+
+
+  it('should add operator1 and operator2 when i click the substraction button ', () => {
+    // Arrange 
+    componentsub.operator1 = 5.0;
+    componentsub.operator2 = 2.5;
+    let substractionButton = fixturesub.debugElement.query(By.css('.substraction-button'));
+
+    // Act
+    substractionButton.triggerEventHandler('click', null);
+
+    // Assert
+    expect(componentsub.result).toBe(2.5);
+
+   });
+
+  it('Should render res in result div', () => {
+    // Arrange
+    componentsub.operator1 = 15;
+    componentsub.operator2 = 5;
+ 
+    // Act
+    componentsub.substraction();
+    fixturesub.detectChanges();
+    
+    let de = fixturesub.debugElement.query(By.css('.result'));
+    let el : HTMLElement = de.nativeElement;
+
+    // Assert
+    expect(el.innerText).toContain('10');
+     
+  });
+
+});
