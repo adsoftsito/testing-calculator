bfd8106f03b77a728cd0f60298fe99323e4921ef
OJEDA ROSAS JONATHAN ARATH <zs20006736@estudiantes.uv.mx>
2022-11-30 04:49:38 -0600

    add resta ui

--- src/app/ui/ui.component.spec.ts
+++ src/app/ui/ui.component.spec.ts
@@ -35,8 +35,6 @@ describe('Ui Addition - Component', () => {
      expect(result).toBe(4);
   });
 
-
-
   it('Should set operator1 model through ngModel', async() => {
     // Arrange 
     await fixture.whenStable();
@@ -100,4 +98,101 @@ describe('Ui Addition - Component', () => {
   });
 
 });
+
+describe('Ui resta - Component', () => {
+  let component: UiComponent;
+  let fixture: ComponentFixture<UiComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ UiComponent ],
+      imports: [FormsModule],
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UiComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('Should call resta method', () => {
+    // Arrange
+    let result = 0;
+    component.operator1 = 3;
+    component.operator2 = 2;
+
+    // Act
+    component.substraction();
+    result = component.result;
+
+    // Assert
+    expect(result).toBe(1);
+ });
+
+ it('Should set operator1 model through ngModel', async() => {
+  // Arrange 
+  await fixture.whenStable();
+  fixture.detectChanges();
+  const inputElement = fixture.debugElement.query(By.css('input[name="operator1"]')).nativeElement;
+
+  // Act 
+  inputElement.value = '2';
+  inputElement.dispatchEvent(new Event('input'));
+  fixture.detectChanges();
+
+  // Assert 
+  expect(component.operator1).toEqual(2);
+ });
+
+ it('Should set operator2 model through ngModel', async() => {
+  // Arrange 
+  await fixture.whenStable();
+  fixture.detectChanges();
+  const inputElement = fixture.debugElement.query(By.css('input[name="operator2"]')).nativeElement;
+
+  // Act 
+  inputElement.value = '71';
+  inputElement.dispatchEvent(new Event('input'));
+  fixture.detectChanges();
+
+  // Assert 
+  expect(component.operator2).toEqual(71);
+});
+
+ it('should restar operator1 and operator2 when i click the resta button ', () => {
+  // Arrange 
+  component.operator1 = 5.0;
+  component.operator2 = 2.5;
+  let restaButton = fixture.debugElement.query(By.css('.substraction-button'));
+
+  // Act
+  restaButton.triggerEventHandler('click', null);
+
+  // Assert
+  expect(component.result).toBe(2.5);
+
+ });
+
+ it('Should render resta in result div', () => {
+  // Arrange
+  component.operator1 = 5;
+  component.operator2 = 5;
+
+  // Act
+  component.addition();
+  fixture.detectChanges();
+  
+  let de = fixture.debugElement.query(By.css('.result'));
+  let el : HTMLElement = de.nativeElement;
+
+  // Assert
+  expect(el.innerText).toContain('0');
+   
+});
+
+});
+
+
 
